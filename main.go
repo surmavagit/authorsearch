2cdd709256c31c4b046756f37117122c8ec31b38
surmavagit <georgesurmava@gmail.com>
2024-01-23 19:04:50 +0100

    split packages

--- main.go
+++ main.go
@@ -1,106 +1,49 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"strings"
+
+	"github.com/surmavagit/authorsearchcli/authorsearch"
 )
 
-type data struct {
-	Description string `json:"name"`
-	AuthorURL   string `json:"href"`
+var marxists = authorsearch.Resource{
+	Name:      "marxists",
+	BaseURL:   "https://www.marxists.org/",
+	QueryURL:  "admin/js/data/authors.json",
+	CacheFile: "cache/marxists.json",
 }
 
-type resource struct {
-	baseURL   string
-	queryURL  string
-	cacheFile string
-	data      []data
-}
-
-var marxists = resource{
-	baseURL:   "https://www.marxists.org/",
-	queryURL:  "admin/js/data/authors.json",
-	cacheFile: "cache/marxists.json",
-}
-
-func init() {
-	_, err := os.Stat("cache")
-	if errors.Is(err, os.ErrNotExist) {
-		err = os.Mkdir("cache", 0755)
-	}
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
+var resources = []authorsearch.Resource{marxists}
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("no search query provided")
-		os.Exit(1)
-	}
-	searchQuery := strings.Join(os.Args[1:], " ")
-
-	err := marxists.loadCache()
+	searchQuery, err := checkInput(os.Args)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	authorLink := marxists.search(searchQuery)
-	if authorLink == "" {
-		fmt.Println("not found")
+	results, err := authorsearch.Search(resources, searchQuery)
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(authorLink)
-}
 
-func (website resource) search(query string) string {
-	for _, a := range website.data {
-		if strings.Contains(a.Description, query) {
-			return website.baseURL + a.AuthorURL
+	for _, r := range results {
+		output := r.Error + r.URL
+		if output == "" {
+			fmt.Println(r.Resource, "not found")
+		} else {
+			fmt.Println(r.Resource, output)
 		}
 	}
-
-	return ""
 }
 
-func (website *resource) loadCache() error {
-	_, err := os.Stat(website.cacheFile)
-	if errors.Is(err, os.ErrNotExist) {
-		err := website.updateCache()
-		if err != nil {
-			return err
-		}
-	} else if err != nil {
-		return err
+func checkInput(query []string) (string, error) {
+	if len(query) <= 1 {
+		return "", errors.New("no search query provided")
 	}
-
-	file, err := os.ReadFile(website.cacheFile)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(file, &website.data)
-	return err
-}
-
-func (website resource) updateCache() error {
-	res, err := http.Get(website.baseURL + website.queryURL)
-	if err != nil {
-		return err
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-
-	err = os.WriteFile(website.cacheFile, body, 0644)
-	return err
+	return strings.Join(query[1:], " "), nil
 }
