278b8a26259dcafb4fa41c37b2e46b59a3ef51e3
surmavagit <georgesurmava@gmail.com>
2024-02-13 14:43:16 +0100

    remove init function, move checking cache directory to main.go

--- main.go
+++ main.go
@@ -17,13 +17,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	cacheDirectory := "cache"
+	err = checkCacheDir(cacheDirectory)
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Exit(1)
+	}
+
 	dataChan := make(chan authorsearch.Resource)
 	wg := sync.WaitGroup{}
 	for _, r := range resources {
 		resource := r
 		wg.Add(1)
 		go func() {
-			dataChan <- resource.SearchResource(searchQuery)
+			dataChan <- resource.SearchResource(searchQuery, cacheDirectory)
 		}()
 	}
 	go func() {
@@ -57,4 +64,26 @@ func checkInput(query []string) (string, error) {
 		return "", errors.New("no search query provided")
 	}
 	return strings.Join(query[1:], " "), nil
+}
+
+func checkCacheDir(directory string) error {
+	info, err := os.Stat(directory)
+
+	if errors.Is(err, os.ErrNotExist) {
+		err = os.Mkdir(directory, 0755)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if !info.IsDir() {
+		return fmt.Errorf("%s is not a directory", directory)
+	}
+
+	return nil
 }
