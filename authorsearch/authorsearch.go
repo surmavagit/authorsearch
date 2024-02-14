73fe96b860bc8c189ad0d47b0e268117f1223875
surmavagit <georgesurmava@gmail.com>
2024-02-14 14:32:51 +0100

    add search by first name and year

--- authorsearch/authorsearch.go
+++ authorsearch/authorsearch.go
@@ -13,15 +13,23 @@ type Resource struct {
 	Name         string
 	BaseURL      string
 	QueryURL     string
-	DataFormat   string
-	DescInParent bool
+	DataFormat   string // json or html
+	DescInParent bool   // Location of the full description in HTML document relative to the <a> tag
 	URLFilter    string // Valid URLs contain this string
+	FirstName    bool
+	Year         bool
 	Results      []authorData
 	Error        error
 }
 
+type Query struct {
+	LastName  string
+	FirstName string
+	Year      string
+}
+
 // SearchResource loads the cached data and searches for the author.
-func (website Resource) SearchResource(query string, cacheDir string) Resource {
+func (website Resource) SearchResource(query Query, cacheDir string) Resource {
 	data, err := website.loadCache(cacheDir)
 	if err != nil {
 		website.Error = err
@@ -30,7 +38,7 @@ func (website Resource) SearchResource(query string, cacheDir string) Resource {
 
 	results := []authorData{}
 	for _, a := range data {
-		if search(a.Description, query) {
+		if website.search(a.Description, query) {
 			results = append(results, a)
 		}
 	}
@@ -38,6 +46,17 @@ func (website Resource) SearchResource(query string, cacheDir string) Resource {
 	return website
 }
 
-func search(authorDesc string, query string) bool {
-	return strings.Contains(authorDesc, query)
+func (website Resource) search(authorDesc string, query Query) bool {
+	if !strings.Contains(authorDesc, query.LastName) {
+		return false
+	}
+
+	if website.FirstName && !strings.Contains(authorDesc, query.FirstName) {
+		return false
+	}
+
+	return !website.Year || strings.Contains(authorDesc, query.Year)
+}
+
+func (website Resource) queryCompatible(query Query) {
 }
