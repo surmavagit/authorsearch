a362ef06a66f482c6948a40b4b221bf168daf03d
surmavagit <georgesurmava@gmail.com>
2024-02-17 13:27:21 +0100

    fix minor errors, add tests

--- main_test.go
+++ main_test.go
@@ -5,10 +5,27 @@ import (
 )
 
 func TestCheckInput(t *testing.T) {
-	inputOne := []string{"test1"}
-	str, err := checkInput(inputOne)
+	emptyInput := []string{}
+	_, err := checkInput(emptyInput)
+	if err == nil {
+		t.Fatal("Expected error, got no error")
+	}
 
-	if str != "" || err == nil {
-		t.Fatalf("Expected: '', error ; Got: '%s', '%v'", str, err)
+	excessiveInput := []string{"a", "b", "c", "d"}
+	_, err = checkInput(excessiveInput)
+	if err == nil {
+		t.Fatal("Expected error, got no error")
+	}
+
+	tooManyNumerics := []string{"1", "2", "a"}
+	_, err = checkInput(tooManyNumerics)
+	if err == nil {
+		t.Fatal("Expected error, got no error")
+	}
+
+	tooManyNames := []string{"a", "b", "c"}
+	_, err = checkInput(tooManyNames)
+	if err == nil {
+		t.Fatal("Expected error, got no error")
 	}
 }
