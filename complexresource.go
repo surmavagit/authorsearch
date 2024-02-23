bc814225dad65cf57cf17b84c2eace6ebe391674
surmavagit <georgesurmava@gmail.com>
2024-02-23 14:17:11 +0100

    add gutenberg resource, refactoring

---
+++ complexresource.go
@@ -0,0 +1,14 @@
+package main
+
+import "errors"
+
+func (website resource) searchComplexResource(query query) resource {
+	// check history
+
+	// query and analyze result
+	website.Error = errors.New("under construction")
+
+	// possibly update history
+
+	return website
+}
