6490fda30c3fea795ba6a23a185277f396ff24d2
surmavagit <georgesurmava@gmail.com>
2024-02-24 23:37:51 +0100

    chore: split functions getCacheFileName, getFullQueryURL, getResource, getQueryString

--- simpleresource.go
+++ simpleresource.go
@@ -11,11 +11,10 @@ type authorData struct {
 
 // searchResource loads the cached data and searches for the author.
 func (website resource) searchResource(query query, cacheDir string) (data []authorData, err error) {
+	cacheFileName := website.getCacheFileName(cacheDir, query.LastName)
 	if website.Complex {
-		return website.searchComplexResource(query, cacheDir)
+		return website.searchComplexResource(query, cacheFileName)
 	}
-
-	cacheFileName := cacheDir + "/" + website.Name + ".json"
 
 	update, err := fileNotExist(cacheFileName)
 	if err != nil {
@@ -23,7 +22,7 @@ func (website resource) searchResource(query query, cacheDir string) (data []aut
 	}
 
 	if update {
-		data, err = website.getResource(cacheDir, cacheFileName)
+		data, err = website.getResource(query)
 	} else {
 		err = loadFileJSON(cacheFileName, &data)
 	}
@@ -32,6 +31,13 @@ func (website resource) searchResource(query query, cacheDir string) (data []aut
 	}
 
 	return website.filterRelevant(data, query), writeFileJSON(cacheFileName, data)
+}
+
+func (website resource) getCacheFileName(cacheDir string, lastName string) string {
+	if !website.Complex {
+		return cacheDir + "/" + website.Name + ".json"
+	}
+	return cacheDir + "/" + website.Name + "_" + strings.ToLower(lastName) + ".json"
 }
 
 func (website resource) filterRelevant(data []authorData, query query) []authorData {
@@ -53,21 +59,46 @@ func (website resource) match(authorDesc string, query query) bool {
 		return false
 	}
 
-	return !website.Year || strings.Contains(authorDesc, query.Year)
+	if website.Year && !strings.Contains(authorDesc, query.Year) {
+		return false
+	}
+
+	return true
 }
 
-// updateCache carries out an http get request and saves the response body
-// into a file
-func (website resource) getResource(cacheDir string, cacheFileName string) ([]authorData, error) {
-	fullURL := website.BaseURL + website.QueryURL
-	body, err := requestURL(fullURL)
+func (website resource) getFullQueryURL(query query) string {
+	if !website.Complex {
+		return website.BaseURL + website.QueryURL
+	}
+
+	queryTerms := []string{query.LastName}
+	if website.FirstName && query.FirstName != "" {
+		queryTerms = append(queryTerms, query.FirstName)
+	}
+	if website.Year && query.Year != "" {
+		queryTerms = append(queryTerms, query.Year)
+	}
+	formatQuery := strings.Join(queryTerms, "+")
+
+	return website.BaseURL + website.QueryURL + formatQuery
+}
+
+// getResource carries out an http get request and gets all the data
+// from the response body
+func (website resource) getResource(query query) ([]authorData, error) {
+	body, err := requestURL(website.getFullQueryURL(query))
+	if err != nil {
+		return []authorData{}, err
+	}
+	data, err := website.readResource(body)
 	if err != nil {
 		return []authorData{}, err
 	}
 
-	data, err := website.readResource(body)
-	if err != nil {
-		return []authorData{}, err
+	// data should be deduped only in case of simple resources, where
+	// creators often provide lists with duplicate values
+	if website.Complex {
+		return data, nil
 	}
 
 	return dedupe(data), nil
